77b94364451b20e63bccc2a75c5cc7d906683c41
Benjol <ben.simkins@gmail.com>
2011-02-28 04:55:22 -0800

    V1.0.2 - Implemented customisation, using HTML5 storage to allow users to create/modify their own messages

--- autoreviewcomments.user.js
+++ autoreviewcomments.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           AutoReviewComments
 // @namespace      benjol
-// @version        1.0.1
+// @version        1.0.2
 // @description    Add pro-forma comments dialog for reviewing (pre-flag)
 // @include        http://*stackoverflow.com/questions*
 // @include        http://*.stackexchange.com/questions*
@@ -20,10 +20,12 @@ function with_jquery(f) {
 
 with_jquery(function ($) {
   $(function () {
-    var siteurl = window.location.hostname;
+    var siteurl = 'http://' + window.location.hostname; //include http in here so we don't get confusion between so and meta.so
     var arr = document.title.split(' - ');
     var sitename = arr[arr.length - 1];
     if(sitename == "Stack Exchange") sitename = arr[arr.length - 2]; //workaround for SE sites..
+    var greeting = 'Welcome to ' + sitename + '! ';
+
     var markup = '                                          \
     <div id="popup" class="popup" style="width:690px; position: absolute; display: block"> \
        <div class="popup-close"><a title="close this popup (or hit Esc)">&#215;</a></div> \
@@ -38,11 +40,13 @@ with_jquery(function ($) {
          </div>                                             \
          <div class="popup-actions">                        \
           <div style="float: left; margin-top: 18px;">      \
-            <a class="popup-actions-cancel">cancel</a>      \
+            <a title="close this popup (or hit Esc)" class="popup-actions-cancel">cancel</a>      \
             <span class="lsep"> | </span>                   \
-            <a class="popup-actions-help" href="http://meta.stackoverflow.com/questions/74194/how-to-review-can-we-agree-on-a-review-policy" target="_blank">info</a>  \
+            <a title="see info about this popup" class="popup-actions-help" href="http://stackapps.com/questions/2116/pro-forma-comments-for-review-educating-users-before-flagging" target="_blank">info</a>  \
             <span class="lsep"> | </span>                   \
             <a class="popup-actions-see">see-through</a>    \
+            <span class="lsep"> | </span>                   \
+            <a title="reset any custom messages" class="popup-actions-reset">reset</a>    \
           </div>                                            \
           <div style="float:right">                         \
             <input class="popup-submit" type="button" disabled="disabled" style="float:none; margin-left: 5px" value="Insert">  \
@@ -51,22 +55,23 @@ with_jquery(function ($) {
        </div>                                               \
     </div>';
 
-    var option = '                                          \
-    <li>                                                    \
-      <input id="$ID$" type="radio" name="commentreview"/>  \
-      <label for="$ID$">                                    \
-        <span class="action-name">$NAME$</span>             \
-        <span class="action-desc">$DESCRIPTION$</span>      \
-      </label>                                              \
+    var option = '                                                          \
+    <li>                                                                    \
+      <input id="comment-$ID$" type="radio" name="commentreview"/>          \
+      <label for="comment-$ID$">                                            \
+        <span id="name-$ID$" class="action-name">$NAME$</span>              \
+        <span id="desc-$ID$" class="action-desc">$DESCRIPTION$</span>       \
+      </label>                                                              \
     </li>';
 
+    //default comments
     var comments = [
-     { Name: "Answers just to say Thanks!", Description: 'Please don\'t add "thanks" as answers. Invest some time in the site and you will gain sufficient <a href="http://$SITEURL$/priveleges">privileges</a> to upvote answers you like, which is the $SITENAME$ way of saying thank you.' },
+     { Name: "Answers just to say Thanks!", Description: 'Please don\'t add "thanks" as answers. Invest some time in the site and you will gain sufficient <a href="$SITEURL$/priveleges">privileges</a> to upvote answers you like, which is the $SITENAME$ way of saying thank you.' },
      { Name: "Nothing but a URL (and isn't spam)", Description: 'Whilst this may theoretically answer the question, <a href="http://meta.stackoverflow.com/questions/8231/are-answers-that-just-contain-links-elsewhere-really-good-answers/8259#8259">it would be preferable</a> to include the essential parts of the answer here, and provide the link for reference.' },
-     { Name: "Requests to OP for further information", Description: 'This is really a comment, not an answer. With a bit more rep, <a href="http://$SITEURL$/privileges/comment">you will be able to post comments</a>. For the moment I\'ve added the comment for you, and I\'m flagging this post for deletion.' },
+     { Name: "Requests to OP for further information", Description: 'This is really a comment, not an answer. With a bit more rep, <a href="$SITEURL$/privileges/comment">you will be able to post comments</a>. For the moment I\'ve added the comment for you, and I\'m flagging this post for deletion.' },
      { Name: "OP using an answer for further information", Description: 'Please use the <em>Post answer</em> button only for actual answers. You should modify your original question to add additional information.' },
-     { Name: "OP adding a new question as an answer", Description: 'If you have another question, please ask it by clicking the <a href="http://$SITEURL$/questions/ask">Ask Question</a> button.' },
-     { Name: "Another user adding a 'Me too!'", Description: 'If you have a NEW question, please ask it by clicking the <a href="http://$SITEURL$/questions/ask">Ask Question</a> button. If you have sufficient reputation, <a href="http://$SITEURL$/privileges/vote-up">you may upvote</a> the question. Alternatively, "star" it as a favorite and you will be notified of any new answers.' },
+     { Name: "OP adding a new question as an answer", Description: 'If you have another question, please ask it by clicking the <a href="$SITEURL$/questions/ask">Ask Question</a> button.' },
+     { Name: "Another user adding a 'Me too!'", Description: 'If you have a NEW question, please ask it by clicking the <a href="$SITEURL$/questions/ask">Ask Question</a> button. If you have sufficient reputation, <a href="$SITEURL$/privileges/vote-up">you may upvote</a> the question. Alternatively, "star" it as a favorite and you will be notified of any new answers.' },
     ];
 
     var weekday_name = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -126,24 +131,116 @@ with_jquery(function ($) {
       return div.find(selector).find('.user-details > a').attr('href').replace(/[^\d]/g, '');
     }
 
+    function getUserInfo(userid, container) {
+      var userinfo = container.find('#userinfo');
+      //http://soapi.info/code/js/stable/soapi-explore-beta.htm
+      $.ajax({
+        type: "GET",
+        url: siteurl.replace('http://', 'http://api.') + '/1.0/users/' + userid + '?jsonp=?',
+        dataType: "jsonp",
+        timeout: 2000,
+        success: function (data) {
+          if(data['users'].length > 0) {
+            var user = data['users'][0];
+            if(isNewUser(user['creation_date'])) {
+              container.find('.action-desc').prepend(greeting);
+            }
+
+            var html = 'User <strong><a href="/users/' + userid + '" target="_blank">' + user['display_name'] + '</a></strong>, \
+                            member <strong>' + datespan(user['creation_date']) + '</strong>,                                        \
+                            last seen <strong>' + lastseen(user['last_access_date']) + '</strong>,                                  \
+                            reputation <strong>' + repNumber(user['reputation']) + '</strong>';
+            userinfo.html(html.replace(/ +/g, ' '));
+          }
+          else userinfo.fadeOutAndRemove();
+        },
+        error: function () { userinfo.fadeOutAndRemove(); }
+      });
+    }
     function isNewUser(date) {
       return (new Date() / 1000) - date < week
     }
 
     function htmlToMarkDown(html) {
-      html = html.replace(/<a href="(.+?)">(.+?)<\/a>/g, '[$2]($1)');
-      return html.replace(/<em>(.+?)<\/em>/g, '*$1*');
+      markdown = html.replace(/<a href="(.+?)">(.+?)<\/a>/g, '[$2]($1)');
+      return markdown.replace(/<em>(.+?)<\/em>/g, '*$1*').replace(/<strong>(.+?)<\/strong>/g, '**$1**');
     }
 
+    function markDownToHtml(markdown) {
+      html = markdown.replace(/\[(.+?)\]\((.+?)\)/g, '<a href="$2">$1</a>');
+      return html.replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>').replace(/\*(.+?)\*/g, '<em>$1</em>');
+    }
+    function ToEditable(el) {
+      var html = el.html();
+      if(html.indexOf('<textarea') > -1) return; //don't want to create a new textarea inside this one!
+      var txt = $('<textarea />').css('height', 2 * el.height())
+                .css('width', el.css('width'))
+                .attr('value', htmlToMarkDown(html).replace(greeting, ''));
+      //This is to stop the input pinching focus when I click inside textarea
+      //Could have done something clever with contentEditable, but this is evil, and it annoys Yi :P
+      BorkFor(el);
+      //save/cancel links to add to textarea
+      var commands = $('<a>save</a>').click(function () { SaveEditable($(this).parent(), html); UnborkFor(el); })
+                      .add('<span class="lsep"> | </span>')
+                      .add($('<a>cancel</a>').click(function () { CancelEditable($(this).parent(), html); UnborkFor(el); }));
+      //set contents of element to textarea with links
+      el.html(txt.add(commands));
+    }
+
+    function BorkFor(el) {
+      var label = el.parent('label');
+      label.attr('for', 'borken');
+    }
+    function UnborkFor(el) {
+      var label = el.parent('label');
+      label.attr('for', label.prev().attr('id'));
+    }
+    function SaveEditable(el, backup) {
+      var html = markDownToHtml(el.find('textarea').attr('value'));
+      if(html != backup) localStorage.setItem(el.attr('id'), html);
+      el.html(html);
+    }
+    function CancelEditable(el, backup) {
+      el.html(backup);
+    }
+    function ResetComments(popup) {
+      $.each(comments, function (index, value) {
+        localStorage.setItem('name-' + index, '');
+        localStorage.setItem('desc-' + index, '');
+        popup.find('#name-' + index).html(value["Name"]);
+        popup.find('#desc-' + index).html(value["Description"].replace(/\$SITENAME\$/g, sitename).replace(/\$SITEURL\$/g, siteurl));
+      });
+    }
     $(".comments-link").each(function () {
       var divid = $(this).attr('id').replace('-link', '');
       $(this).click(function () {
-        var newspan = $('<span class="lsep"> | </span>').add($('<a>auto</a>').click(function () {
+        if($('#' + divid).find('.comment-auto-link').length > 0) return; //don't create auto link if already there
+        var newspan = $('<span class="lsep"> | </span>').add($('<a class="comment-auto-link">auto</a>').click(function () {
           //Create popup and wire-up the functionality
           var popup = $(markup);
           popup.find('.popup-close').click(function () { popup.fadeOutAndRemove(); });
+
+          //create/add options
+          $.each(comments, function (index, value) {
+            var name = localStorage['name-' + index] || value["Name"];
+            var desc = localStorage['desc-' + index] || value["Description"].replace(/\$SITENAME\$/g, sitename).replace(/\$SITEURL\$/g, siteurl);
+            var opt = option.replace(/\$ID\$/g, index)
+                            .replace("$NAME$", name)
+                            .replace("$DESCRIPTION$", desc);
+            popup.find('.action-list').append(opt);
+          });
+          popup.find('label > span').dblclick(function () { ToEditable($(this)); });
+          //add click handler to radio buttons
+          popup.find('input:radio').click(function () {
+            popup.find('.popup-submit').attr("disabled", ""); //enable submit button
+            //unset/set selected class
+            $(this).parents('ul').find(".action-selected").removeClass("action-selected");
+            $(this).parent().addClass('action-selected');
+          });
+
+          //Add handlers for command links
           popup.find('.popup-actions-cancel').click(function () { popup.fadeOutAndRemove(); });
-          //set opacity on mousedown/mouseup of 'see-through' link
+          popup.find('.popup-actions-reset').click(function () { ResetComments(popup); });
           popup.find('.popup-actions-see').hover(function () {
             popup.fadeTo('fast', '0.4').find('.popup-active-pane').fadeTo('fast', '0.0')
           }, function () {
@@ -156,52 +253,14 @@ with_jquery(function ($) {
             $('#' + divid).find('textarea').attr('value', markdown).focus();  //focus provokes character count test
             popup.fadeOutAndRemove();
           });
-          //create/add options
-          $.each(comments, function (index, value) {
-            var opt = option.replace(/\$ID\$/g, "comment-" + index)
-                            .replace("$NAME$", value["Name"])
-                            .replace("$DESCRIPTION$", value["Description"].replace(/\$SITENAME\$/g, sitename).replace(/\$SITEURL\$/g, siteurl));
-            popup.find('.action-list').append(opt);
-          });
-          //add click handler to radio buttons
-          popup.find('input:radio').click(function () {
-            popup.find('.popup-submit').attr("disabled", ""); //enable submit button
-            //unset/set selected class
-            $(this).parents('ul').find(".action-selected").removeClass("action-selected");
-            $(this).parent().addClass('action-selected');
-          });
-          //add popup and scroll into view
+          //add popup and center on screen
           $('#' + divid).append(popup);
+          popup.center();
 
           //Get user info and inject
           var userid = getUserId($(this));
-          var userinfo = popup.find('#userinfo');
-          //http://soapi.info/code/js/stable/soapi-explore-beta.htm
-          $.ajax({
-            type: "GET",
-            url: 'http://api.' + siteurl + '/1.0/users/' + userid + '?jsonp=?',
-            dataType: "jsonp",
-            timeout: 2000,
-            success: function (data) {
-              if(data['users'].length > 0) {
-                var user = data['users'][0];
-                if(isNewUser(user['creation_date'])) {
-                  var greeting = 'Welcome to ' + sitename + '! ';
-                  popup.find('.action-desc').prepend(greeting);
-                }
+          getUserInfo(userid, popup);
 
-                var html = 'User <strong><a href="/users/' + userid + '" target="_blank">' + user['display_name'] + '</a></strong>, \
-                            member <strong>' + datespan(user['creation_date']) + '</strong>,                                        \
-                            last seen <strong>' + lastseen(user['last_access_date']) + '</strong>,                                  \
-                            reputation <strong>' + repNumber(user['reputation']) + '</strong>';
-                userinfo.html(html);
-              }
-              else userinfo.fadeOutAndRemove();
-            },
-            error: function () { userinfo.fadeOutAndRemove(); }
-          });
-
-          popup.center();
         }));
         $('#' + divid).find('.comment-help-link').parent().append(newspan);
       });
