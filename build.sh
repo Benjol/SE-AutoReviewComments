a5be7bde3f2f1224cbca49adc2e47859d3d07063
Caleb Maclennan <caleb@alerque.com>
2014-03-04 09:46:42 +0200

    fix build script to work on Windows

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-case $(getconf LONG_BIT) in
+# The Windows git bash environment doesn't have getconf, but
+# it's safe to assume that if we don't have the command that
+# we don't need to pass anything special to ant. Likewise the
+# ant build target assumes 32 bit unless told otherwise.
+case $(hash getconf 2>/dev/null && getconf LONG_BIT) in
     64)
         ant -Darch=64 "$@"
         ;;
