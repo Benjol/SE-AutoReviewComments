66fc14e99ddc18408589d35e70b0d93224a7f109
Oliver Salzburg <oliver.salzburg@gmail.com>
2014-06-11 22:16:19 +0200

    New Look and Feel The major changes include: - All elements have been repositioned to even out margins. - The radio buttons have been hidden. - Clicking *anywhere* on the name or description of an action will select it. - All action link (save/cancel/import/...) now share common styles and are positioned similarly.

--- src/autoreviewcomments.user.js
+++ src/autoreviewcomments.user.js
@@ -181,7 +181,7 @@ with_jquery(function ($) {
     //Show textarea in front of popup to import/export all comments (for other sites or for posting somewhere)
     function ImportExport(popup) {
       var tohide = popup.find('#main');
-      var div = $('<div><textarea/><a class="jsonp">jsonp</a><span class="lsep"> | </span><a class="save">save</a><span class="lsep"> | </span><a class="cancel">cancel</a></div>');
+      var div = $('<div><textarea/><div class="actions"><a class="jsonp">jsonp</a><span class="lsep"> | </span><a class="save">save</a><span class="lsep"> | </span><a class="cancel">cancel</a></div></div>');
       //Painful, but shortest way I've found to position div over the tohide element
       div.css({ position: 'absolute', left: tohide.position().left, top: tohide.position().top,
         width: tohide.css('width'), height: tohide.css('height'), background: 'white'
@@ -194,7 +194,7 @@ with_jquery(function ($) {
         txt += '###' + name + '\n' + htmlToMarkDown(desc) + '\n\n'; //the leading ### makes prettier if pasting to markdown, and differentiates names from descriptions
       }
 
-      div.find('textarea').width('100%').height('95%').val(txt);
+      div.find('textarea').val(txt);
       div.find('.jsonp').click(function () {
         var txt = 'callback(\n[\n';
         for(var i = 0; i < GetStorage("commentcount"); i++) {
@@ -257,17 +257,18 @@ with_jquery(function ($) {
       var backup = el.html();
       var html = Tag(el.html().replace(greeting, ''));  //remove greeting before editing..
       if(html.indexOf('<textarea') > -1) return; //don't want to create a new textarea inside this one!
-      var txt = $('<textarea />').css('height', 2 * el.height())
-                .css('width', el.css('width'))
-                .val(htmlToMarkDown(html));
+      var txt = $('<textarea />').val(htmlToMarkDown(html));
 
       BorkFor(el); //this is a hack
       //save/cancel links to add to textarea
-      var commands = $('<a>save</a>').click(function () { SaveEditable($(this).parent()); UnborkFor(el); })
+      var actions = $('<div class="actions">');
+      var commands = $('<a>save</a>').click(function () { SaveEditable($(this).parent().parent()); UnborkFor(el); })
                       .add('<span class="lsep"> | </span>')
-                      .add($('<a>cancel</a>').click(function () { CancelEditable($(this).parent(), backup); UnborkFor(el); }));
+                      .add($('<a>cancel</a>').click(function () { CancelEditable($(this).parent().parent(), backup); UnborkFor(el); }));
+      actions.append(commands);
+
       //set contents of element to textarea with links
-      el.html(txt.add(commands));
+      el.html(txt.add(actions));
     }
 
     //This is to stop the input pinching focus when I click inside textarea
